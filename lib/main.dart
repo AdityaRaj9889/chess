a0c29797f99316dfff6aebc481d1d66d7e541a2a
Aditya <adityaraj9889@gmail.com>
2025-02-09 23:41:34 +0530

    bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:chess_game/screens/gameBoard.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    DevicePreview(
-      enabled: true,
-      tools: const [
-        ...DevicePreview.defaultTools,
-      ],
-      builder: (context) => const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
